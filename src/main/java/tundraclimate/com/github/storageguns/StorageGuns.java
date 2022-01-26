1cc47693855c627fa1dfaac4d81f62e7f9b96d62
BuildTools <unconfigured@null.spigotmc.org>
2022-01-27 01:19:05 +0900

    source

---
+++ src/main/java/tundraclimate/com/github/storageguns/StorageGuns.java
@@ -0,0 +1,10 @@
+package tundraclimate.com.github.storageguns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StorageGuns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
