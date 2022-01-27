a9545dfc30bc8ba1b9cb3328d6d30a83b442b30b
BuildTools <unconfigured@null.spigotmc.org>
2022-01-27 16:33:29 +0900

    GunType Enum

--- src/main/java/tundraclimate/com/github/storageguns/GunsEnum.java
+++ src/main/java/tundraclimate/com/github/storageguns/GunsEnum.java
@@ -1,4 +1,30 @@
 package tundraclimate.com.github.storageguns;
 
+
+import org.bukkit.Material;
+
 public enum GunsEnum {
+    TNT_GUN(2,Material.TNT,Material.GOLDEN_HOE);
+
+    private final int ammoSize;
+    private final Material ammoType;
+    private final Material gunType;
+
+    GunsEnum(int size,Material ammoType, Material gunType) {
+        this.ammoSize = size;
+        this.ammoType = ammoType;
+        this.gunType = gunType;
+    }
+
+    public int getAmmoSize() {
+        return ammoSize;
+    }
+
+    public Material getAmmoType() {
+        return ammoType;
+    }
+
+    public Material getGunType() {
+        return gunType;
+    }
 }
